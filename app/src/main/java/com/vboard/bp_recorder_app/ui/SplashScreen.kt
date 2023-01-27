f041f2ff7f9e4e93e9458feeb6340a4784c332c7
Ali Raxa <anndroiderr9@gmail.com>
2023-01-27 23:41:41 +0500

    working on bug fixing

--- app/src/main/java/com/vboard/bp_recorder_app/ui/SplashScreen.kt
+++ app/src/main/java/com/vboard/bp_recorder_app/ui/SplashScreen.kt
@@ -1,12 +1,31 @@
 package com.vboard.bp_recorder_app.ui
 
+import android.annotation.SuppressLint
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.vboard.bp_recorder_app.R
+import com.vboard.bp_recorder_app.utils.showToast
 
+
+@SuppressLint("CustomSplashScreen")
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+
+        Handler(Looper.myLooper()!!).postDelayed({
+            try {
+                startActivity(Intent(this, MainActivity::class.java))
+            } catch (exception: ActivityNotFoundException) {
+                this.showToast(getString( R.string.no_activity_found_exception))
+            }
+
+
+        }, 3000)
     }
 }
