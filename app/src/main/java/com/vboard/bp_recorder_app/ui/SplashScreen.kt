48f179fd1884864e2372ab7cda4bcc019e814d83
Raxa-AndroidDev <anndroiderr9@gmail.com>
2023-01-30 09:13:59 +0500

    working on hear rate module

--- app/src/main/java/com/vboard/bp_recorder_app/ui/SplashScreen.kt
+++ app/src/main/java/com/vboard/bp_recorder_app/ui/SplashScreen.kt
@@ -1,12 +1,22 @@
 package com.vboard.bp_recorder_app.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.vboard.bp_recorder_app.R
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+            finish()
+        }, 3000)
     }
 }
